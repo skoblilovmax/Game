f052327e461137229f603e4344b593da36172258
skoblilovmax <skoblilovmax@mail.ru>
2020-04-26 16:42:14 +0300

    Responsive width and height of buttons for different resolutions

--- Assets/Scripts/MyGUI.cs
+++ Assets/Scripts/MyGUI.cs
@@ -16,15 +16,15 @@ public class MyGUI : MonoBehaviour
     {
         if (isLose)
         {
-            GUI.Box(new Rect(870, 200, 200, 30), "You lose", customBox);
+            GUI.Box(new Rect(Screen.width / 2 - 100, Screen.height / 4, 200, 30), "You lose", customBox);
 
-            if (GUI.Button(new Rect(870, 500, 200, 50), "PLAY", customButton))
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, 2 * Screen.height / 4, 200, 50), "PLAY", customButton))
             {
                 Time.timeScale = 1;
                 SceneManager.LoadScene(1);
             }
 
-            if (GUI.Button(new Rect(870, 600, 200, 50), "QUIT", customButton))
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, 2 * Screen.height / 4 + 100, 200, 50), "QUIT", customButton))
             {
                 Application.Quit();
             }
@@ -32,15 +32,15 @@ public class MyGUI : MonoBehaviour
 
         if (isWin)
         {
-            GUI.Box(new Rect(870, 200, 200, 30), "You win", customBox);
+            GUI.Box(new Rect(Screen.width / 2 - 100, Screen.height / 4, 200, 30), "You win", customBox);
 
-            if (GUI.Button(new Rect(870, 500, 200, 50), "PLAY", customButton))
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, 2 * Screen.height / 4, 200, 50), "PLAY", customButton))
             {
                 Time.timeScale = 1;
                 SceneManager.LoadScene(1);
             }
 
-            if (GUI.Button(new Rect(870, 600, 200, 50), "QUIT", customButton))
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, 2 * Screen.height / 4 + 100, 200, 50), "QUIT", customButton))
             {
                 Application.Quit();
             }
