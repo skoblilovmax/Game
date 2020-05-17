477d50b8471211102c6daf2902647b7975fd439c
skoblilovmax <skoblilovmax@mail.ru>
2020-05-17 22:42:18 +0300

    Added smooth increase and decrease objects

--- Assets/Scripts/EnemyCollision.cs
+++ Assets/Scripts/EnemyCollision.cs
@@ -1,13 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class EnemyCollision : MonoBehaviour
 {
     public List<GameObject> enemies;
     public AudioClip deadSound;
-
+    public GameObject player;
+    public GameObject spawnerLeft;
+    
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        spawnerLeft = GameObject.Find("SpawnerLeft");
+    }
+    
     void OnCollisionEnter2D(Collision2D other)
     {
         if ((other.gameObject.tag == "Enemy")||(other.gameObject.tag=="Player"))
@@ -21,21 +29,58 @@ public class EnemyCollision : MonoBehaviour
             {
                 firstEnemyScaleX = Mathf.Sqrt(Mathf.Pow(firstEnemyScaleX, 2f) + Mathf.Pow(secondEnemyScaleX, 2f));
                 firstEnemyScaleY = Mathf.Sqrt(Mathf.Pow(firstEnemyScaleY, 2f) + Mathf.Pow(secondEnemyScaleY, 2f));
-                transform.localScale = new Vector2(firstEnemyScaleX, firstEnemyScaleY);
+                StartCoroutine(ScaleIncreaseAnimation(0.2f, firstEnemyScaleX, firstEnemyScaleY));
 
                 if (other.gameObject.tag == "Player")
                 {
-                    Time.timeScale = 0;
-                    GameObject.Find("Message").GetComponent<MyGUI>().isLose = true;
                     AudioSource.PlayClipAtPoint(deadSound, transform.position);
+                    StartCoroutine("LoseCoroutine");
                 }
-
-                enemies = GameObject.Find("SpawnerLeft").GetComponent<Spawn>().enemies;
+                enemies = spawnerLeft.GetComponent<Spawn>().enemies;
                 enemies.Remove(other.gameObject);
-                Destroy(other.gameObject);
+                StartCoroutine(ScaleDecreaseAnimation(0.2f, other.gameObject));
 
-                GameObject.FindGameObjectWithTag("Player").GetComponent<EditColor>().ChangeEnemyColor(gameObject);
+                player.GetComponent<EditColor>().ChangeEnemyColor(gameObject);
             }
         }
     }
+
+    IEnumerator LoseCoroutine()
+    {
+        yield return new  WaitForSeconds(0.3f);
+        Time.timeScale = 0;
+        GameObject.Find("Message").GetComponent<MyGUI>().isLose = true;
+    }
+    
+    IEnumerator ScaleIncreaseAnimation(float time, float x, float y)
+    {
+        float i = 0;
+        float rate = 1 / time;
+    
+        Vector2 fromScale = transform.localScale;
+        Vector2 toScale = new Vector2(x, y);
+        while (i<1)
+        {
+            i += Time.deltaTime * rate;
+            transform.localScale = Vector2.Lerp(fromScale, toScale, i);
+            yield return 0;
+        }
+    }
+    
+    IEnumerator ScaleDecreaseAnimation(float time, GameObject obj)
+    {
+        float i = 0;
+        float rate = 1 / time;
+
+        Vector2 fromScale = obj.transform.localScale;
+        Vector2 toScale = new Vector2(0, 0);
+        while (i<1)
+        {
+            i += Time.deltaTime * rate;
+            obj.transform.localScale = Vector2.Lerp(fromScale, toScale, i);
+            yield return 0;
+        }
+        Destroy(obj.gameObject);
+    }
+
 }
--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -7,6 +7,12 @@ public class PlayerCollision : MonoBehaviour
 {
     public List<GameObject> enemies;
     public AudioClip eatSound;
+    public GameObject spawnerLeft;
+
+    void Start()
+    {
+        spawnerLeft = GameObject.Find("SpawnerLeft");
+    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -21,17 +27,17 @@ public class PlayerCollision : MonoBehaviour
             {
                 playerScaleX = Mathf.Sqrt(Mathf.Pow(playerScaleX, 2f) + Mathf.Pow(enemyScaleX, 2f));
                 playerScaleY = Mathf.Sqrt(Mathf.Pow(playerScaleY, 2f) + Mathf.Pow(enemyScaleY, 2f));
-                transform.localScale = new Vector2(playerScaleX, playerScaleY);
+                StartCoroutine(ScaleIncreaseAnimation(0.2f, playerScaleX, playerScaleY));
 
-                enemies = GameObject.Find("SpawnerLeft").GetComponent<Spawn>().enemies;
+                enemies = spawnerLeft.GetComponent<Spawn>().enemies;
                 enemies.Remove(other.gameObject);
-                Destroy(other.gameObject);
+                StartCoroutine(ScaleDecreaseAnimation(0.2f, other.gameObject));
 
                 AudioSource.PlayClipAtPoint(eatSound, transform.position);
 
                 foreach (var enemy in enemies)
                 {
-                    GameObject.FindGameObjectWithTag("Player").GetComponent<EditColor>().ChangeEnemyColor(enemy);
+                    gameObject.GetComponent<EditColor>().ChangeEnemyColor(enemy);
                 }
 
                 float areaSum = 0;
@@ -43,10 +49,47 @@ public class PlayerCollision : MonoBehaviour
 
                 if((Mathf.PI * Mathf.Pow(gameObject.transform.localScale.x, 2) / 4) > areaSum)
                 {
-                    Time.timeScale = 0;
-                    GameObject.Find("Message").GetComponent<MyGUI>().isWin = true;
+                    StartCoroutine("WinCoroutine");
                 }
             }
         }
+    }
+    
+    IEnumerator WinCoroutine()
+    {
+        yield return new  WaitForSeconds(0.3f);
+        Time.timeScale = 0;
+        GameObject.Find("Message").GetComponent<MyGUI>().isWin = true;
+    }
+
+    IEnumerator ScaleIncreaseAnimation(float time, float x, float y)
+    {
+        float i = 0;
+        float rate = 1 / time;
+
+        Vector2 fromScale = transform.localScale;
+        Vector2 toScale = new Vector2(x, y);
+        while (i<1)
+        {
+            i += Time.deltaTime * rate;
+            transform.localScale = Vector2.Lerp(fromScale, toScale, i);
+            yield return 0;
+        }
+    }
+    
+    IEnumerator ScaleDecreaseAnimation(float time, GameObject obj)
+    {
+        float i = 0;
+        float rate = 1 / time;
+
+        Vector2 fromScale = obj.transform.localScale;
+        Vector2 toScale = new Vector2(0, 0);
+        while (i<1)
+        {
+            i += Time.deltaTime * rate;
+            obj.transform.localScale = Vector2.Lerp(fromScale, toScale, i);
+            yield return 0;
+        }
+        Destroy(obj.gameObject);
     }
 }
